e9b261f4c159b1677a0306d73a63d0bdf33b55f5
Kurt <Kurt@Merbeth.io>
2022-03-02 13:45:14 +0100

    added proxy

---
+++ contracts/Proxy/TransparentProxy.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.4;
+
+import "./openzeppelin/transparent/TransparentUpgradeableProxy.sol";
+
+contract TransparentProxy is TransparentUpgradeableProxy {
+    constructor(address logic, address admin, bytes memory data) payable TransparentUpgradeableProxy(logic, admin, data) {}
+}
