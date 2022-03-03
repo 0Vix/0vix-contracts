47a5f067ce3bc20e4b78923878e37f5990ddf734
Kurt <Kurt@Merbeth.io>
2022-03-03 19:01:45 +0100

    started test cases

---
+++ contracts/mocks/OracleMock.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.5.17;
+
+contract OracleMock {
+    mapping(address => uint256) prices;
+
+    constructor() public {}
+    
+    function getUnderlyingPrice(address oToken) public view returns (uint256) {
+        return prices[oToken];
+    }
+
+    function setPrice(address token, uint256 price) external {
+        prices[token] = price;
+    }
+}
