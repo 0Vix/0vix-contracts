ec577980039a2a2a699da24f976552b5cc1d236b
ethkilla <ethkilla@yandex.ru>
2022-03-14 03:15:36 +0300

    Change ActionPaused to ActionPausedGlobally

--- contracts/Comptroller.sol
+++ contracts/Comptroller.sol
@@ -66,7 +66,7 @@ contract Comptroller is
     event NewPauseGuardian(address oldPauseGuardian, address newPauseGuardian);
 
     /// @notice Emitted when an action is paused globally
-    event ActionPaused(string action, bool pauseState);
+    event ActionPausedGlobally(string action, bool pauseState);
 
     /// @notice Emitted when an action is paused on a market
     event ActionPaused(IOToken oToken, string action, bool pauseState);
@@ -1405,7 +1405,7 @@ contract Comptroller is
         require(msg.sender == admin || state, "only admin can unpause");
 
         transferGuardianPaused = state;
-        emit ActionPaused("Transfer", state);
+        emit ActionPausedGlobally("Transfer", state);
         return state;
     }
 
@@ -1414,7 +1414,7 @@ contract Comptroller is
         require(msg.sender == admin || state, "only admin can unpause");
 
         seizeGuardianPaused = state;
-        emit ActionPaused("Seize", state);
+        emit ActionPausedGlobally("Seize", state);
         return state;
     }
 
