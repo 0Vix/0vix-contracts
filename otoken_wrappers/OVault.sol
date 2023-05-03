0f576292a3ec0b6c664b07bb6f5ec9adb4828a3b
dlockbot <null>
2023-05-03 22:36:44 +0000

    🔄 Synced local './' with remote 'contracts/'

---
+++ otoken_wrappers/OVault.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.1;
+
+import "yield-daddy/compound/CompoundERC4626.sol";
+
+contract OVault is CompoundERC4626 {
+   constructor(
+       ERC20 asset_, ERC20 comp_, ICERC20 cToken_, address rewardRecipient_, IComptroller comptroller_
+    ) CompoundERC4626(asset_, comp_, cToken_, rewardRecipient_, comptroller_) {
+        symbol =  string(abi.encodePacked("wo", asset.symbol()));
+        name = string(abi.encodePacked("ERC4626-Wrapped 0VIX ", asset.name()));
+    }
+
+}
